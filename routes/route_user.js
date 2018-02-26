31f23203134d5a52e0925bcc5386b4b567014fff
unknown <seanboerger@usa.net>
2018-02-26 01:34:51 -0800

    yee

--- routes/route_user.js
+++ routes/route_user.js
@@ -349,7 +349,7 @@ function updateUser(req, res, next)
                         db.storeObject(userReply.id, tempuser, (reply) => 
                         {
                             retVal.data.passwordChanged = false;
-                            res.send(JSON.stringify(retVal));    
+                            //res.send(JSON.stringify(retVal));    
                         });
                     }
                 }
